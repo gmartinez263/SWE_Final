89167f417f9b7df37cad52e57d5c4c78a6b8d169
Gretel Castillo <gretelcastillo@Gretels-MacBook-Pro.local>
2025-04-15 18:18:36 -0700

    frontend leaflet carson map

--- Engineering/map.js
+++ Engineering/map.js
@@ -1,7 +1,18 @@
 // code from https://leafletjs.com/examples/quick-start/
-let map = L.map('map').setView([51.505, -0.09], 13);
+                                //latitude, longitude, zoom view
+let map = L.map('map').setView([33.8314, -118.2812], 13); // Center on Carson
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// Lock view to Carson area
+let bounds = L.latLngBounds(
+    [33.79, -118.33],  // Southwest corner
+    [33.87, -118.23]   // Northeast corner
+);
+map.setMaxBounds(bounds);
+map.on('drag', function() {
+    map.panInsideBounds(bounds, { animate: false });
+});
